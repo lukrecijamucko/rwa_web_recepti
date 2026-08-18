3bf77b4bcc563cbe31f5022e160e50c9fa507087
petar-rupcic <petar.rupcic2000@gmail.com>
2026-08-18 20:43:21 +0200

    Dodana baza, SQLAlchemy i Migrate

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+from config import Config
+from extensions import db, migrate
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route("/")
+    def home():
+        return "Aplikacija radi!"
+
+    return app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
